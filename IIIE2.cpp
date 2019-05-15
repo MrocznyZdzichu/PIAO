8db488c67f5ee39b01f609816ff9429e7a80e985
Max <maksymzawrotny@wp.pl>
2019-05-15 21:25:43 +0200

    computing planes area done

--- IIIE2.cpp
+++ IIIE2.cpp
@@ -9,19 +9,19 @@
 using namespace ogx;
 using namespace ogx::Data;
 
-
 struct IIIE2 : public ogx::Plugin::EasyMethod
 {
 	// parameters
 	Data::ResourceID	m_node_id;
-	float				maxDist;
-	float				neighSize;
+	float neighSize = 20;
+	float maxDist = 0.2;
 #pragma region params,constructor,init
 	// constructor
 	IIIE2() : EasyMethod(L"Maksym Zawrotny", L"Show planes and compute thier area")
 	{
 	}
 
+	// method computing point's distance to plane: normal*xyz + offset
 	float PlanePointDist(Clouds::Point3D normal, Data::Clouds::Point3D point, float offset)
 	{
 		return fabs(normal(0, 0) * point(0, 0) +
@@ -29,12 +29,81 @@ struct IIIE2 : public ogx::Plugin::EasyMethod
 			normal(2, 0) * point(2, 0) +
 			offset);
 	}
+
+	Eigen::Matrix<float, 3, 3> concVector(Clouds::Point3D u,
+										  Clouds::Point3D v,
+										  Clouds::Point3D w)
+	{
+		Eigen::Matrix<float, 3, 3> originBase;
+		originBase(0, 0) = u(0, 0);
+		originBase(0, 1) = v(0, 0);
+		originBase(0, 2) = w(0, 0);
+		originBase(1, 0) = u(1, 0);
+		originBase(1, 1) = v(1, 0);
+		originBase(1, 2) = w(1, 0);
+		originBase(2, 0) = u(2, 0);
+		originBase(2, 1) = v(2, 0);
+		originBase(2, 2) = w(2, 0);
+
+		return originBase;
+	}
+
+	Clouds::Point3D transformPoint(Clouds::Point3D point,
+								   Clouds::Point3D u,
+								   Clouds::Point3D v,
+								   Clouds::Point3D w)
+	{
+		return  concVector(u, v, w).inverse() * point;
+	}
+	// method computing plane approx area
+	float planeArea(std::vector<Clouds::Point3D> plane)
+	{
+		// compute new coords' vuw (w is normal and uv lies in plane)
+		auto fittedPlane = Math::CalcBestPlane3D(plane.begin(), plane.end());
+		Clouds::Point3D normal = fittedPlane.normal().cast<StoredPoint3D::Scalar>();
+		Clouds::Point3D u = Clouds::Point3D(0.33, 0.66, 0.66);
+		Clouds::Point3D v = u.cross(normal);
+	
+
+		// transform points xyz into new line-space base
+		for (int i = 0; i < plane.size(); i++)
+		{
+			Clouds::Point3D newPoint = transformPoint(plane[i], u, v, normal);
+			plane[i] = newPoint;
+		}
+
+		// compute range along u and v axis
+		float minU = plane[0](0, 0);
+		float maxU = plane[0](0, 0);
+		float minV = plane[0](1, 0);
+		float maxV = plane[0](1, 0);
+
+		for (const auto& point : plane)
+		{
+			if (point(0, 0) < minU)
+				minU = point(0, 0);
+
+			if (point(1, 0) < minV)
+				minV = point(1, 0);
+
+			if (point(0, 0) > maxU)
+				maxU = point(0, 0);
+
+			if (point(1, 0) > maxV)
+				maxV = point(1, 0);
+		}
+
+		float DU = maxU - minU;
+		float DV = maxV - minV;
+
+		// compute plane area as a mean of areas of cicrumscribed retangle area and inscribed elipse
+		// approx method but in keeps performance quite good
+		return (PI * 0.25 * DU * DV + DU * DV) / 2;
+	}
 	// add input/output parameters
 	virtual void DefineParameters(ParameterBank& bank)
 	{
 		bank.Add(L"node_id", m_node_id).AsNode();
-		bank.Add(L"neighbors count", neighSize);
-		bank.Add(L"max point distance to fir plane", maxDist);
 	}
 
 	bool Init(Execution::Context& context)
@@ -79,63 +148,62 @@ struct IIIE2 : public ogx::Plugin::EasyMethod
 			points_all.SetLayerVals(segmentNumbers, *segmentsLayer);
 #pragma endregion
 
+#pragma region SegmentPointCloudIntoPlanes
 			ogx::StoredReal		groupNumber = 1;
-			int					iterations = 0;
 			//prepare iterators and ranges to sweep through pc
 			auto xyzRange = Clouds::RangeLocalXYZConst(points_all);
+			auto xyz = xyzRange.begin();				
 			auto segNumRange = Clouds::RangeLayer(points_all, *segmentsLayer);
-			auto xyz = xyzRange.begin();
 			auto segNum = segNumRange.begin();
-		
-#pragma region SegmentPointCloudIntoPlanes
+
 			for (; xyz != xyzRange.end(); xyz++, segNum++)
 			{
-				// go through all points without plane-segment yet
-				// segNum is reference to float so to avoid float == i am doing it this way
-				
-				if (fabs(*segNum) < 0.5)
+
+				// I tried segmentation by growth but updating segNumRange killed performance :(			
+				// define KNN kernel and find neighbors
+				Clouds::KNNSearchKernel knnKernel(Math::Point3D::Zero(), neighSize);
+				Clouds::PointsRange neighborhood;
+				knnKernel.GetPoint() = (*xyz).cast<Math::Point3D::Scalar>();
+				cloud.GetAccess().FindPoints(knnKernel, neighborhood);
+
+				//get xyz coords of neighbors
+				Data::Clouds::RangeLocalXYZConst neighbor_xyz(neighborhood);
+
+				//fit plane 
+				auto fittedPlane = Math::CalcBestPlane3D(neighbor_xyz.begin(), neighbor_xyz.end());
+				Clouds::Point3D normal = fittedPlane.normal().cast<StoredPoint3D::Scalar>();
+				float offset = fittedPlane.offset();
+
+				//compute max point distance
+				ogx::Real maxPointDistance = 0;
+				ogx::Real currentDistance;
+
+				//plane is fit so its distance to each point is equal :( so I break after one iteration
+				for (auto neighXYZ : neighbor_xyz)
 				{
-					// define KNN kernel and find neighbors
-					Clouds::KNNSearchKernel knnKernel(Math::Point3D::Zero(), neighSize);
-					Clouds::PointsRange neighborhood;
-					knnKernel.GetPoint() = (*xyz).cast<Math::Point3D::Scalar>();
-					cloud.GetAccess().FindPoints(knnKernel, neighborhood);
+					currentDistance = PlanePointDist(normal, *xyz, offset);
+					currentDistance > maxPointDistance
+						? maxPointDistance = currentDistance
+						: maxPointDistance = maxPointDistance;
+					break;
+				}
 
-					//get xyz coords of neighbors
-					Data::Clouds::RangeLocalXYZConst neighbor_xyz(neighborhood);
+				//if neighborhood is valid and big enough, assign new semgent number
+				if (maxPointDistance <= maxDist / 100)
+				{
+					std::vector<ogx::StoredReal> segNumbers;
 
-					//fit plane 
-					auto fittedPlane = Math::CalcBestPlane3D(neighbor_xyz.begin(), neighbor_xyz.end());
-					Clouds::Point3D normal = fittedPlane.normal().cast<StoredPoint3D::Scalar>();
-					float offset = fittedPlane.offset();
+					for (int i = 0; i < neighSize; i++)
+						segNumbers.push_back(groupNumber);
 
-					//compute max point distance
-					ogx::Real maxPointDistance = 0;
-					ogx::Real currentDistance;
-
-					for (auto neighXYZ : neighbor_xyz)
-					{
-						currentDistance = PlanePointDist(normal, *xyz, offset);
-						currentDistance > maxPointDistance
-							? maxPointDistance = currentDistance
-							: maxPointDistance = maxPointDistance;
-					}
-
-					//if neighborhood is valid and big enough, assign new semgent number
-					if (maxPointDistance <= maxDist/100)
-					{
-						std::vector<ogx::StoredReal> segNumbers;
-
-						for (int i = 0; i < neighSize; i++)
-							segNumbers.push_back(groupNumber);
-
-						neighborhood.SetLayerVals(segNumbers, *segmentsLayer);
-						groupNumber++;
-					}
+					neighborhood.SetLayerVals(segNumbers, *segmentsLayer);
+					groupNumber++;
 				}
 			}
+
 #pragma endregion
 
+#pragma region SelectingPlanesOnGUI
 			//go back with segment number iterator and prepare state iterator
 			auto segNumRange2 = Clouds::RangeLayer(points_all, *segmentsLayer);
 			auto segNum2 = segNumRange2.begin();
@@ -146,6 +214,35 @@ struct IIIE2 : public ogx::Plugin::EasyMethod
 			for (; segNum2 != segNumRange2.end(); segNum2++, state++)
 				if (fabs(*segNum2) > 0.5)
 					state->set(Clouds::PS_SELECTED);
+#pragma endregion
+
+#pragma region ComputingFlatSurfacesArea
+			// create vectors for each plane
+			std::vector<std::vector<Clouds::Point3D> > planePoints;
+			planePoints.resize(groupNumber - 1);
+
+			segNum2 = segNumRange2.begin();
+			xyz = xyzRange.begin();
+
+			// get points assigned to each plane
+			for (; xyz != xyzRange.end(); segNum2++, xyz++)
+			{
+				if (fabs(*segNum2 - 0) > 0.5)
+				{
+					planePoints[int(std::round(*segNum2-1))].push_back(*xyz);
+				}
+			}
+
+			float totalArea = 0;
+			// count each plane area and add it to totalArea
+			for (int i = 0; i < groupNumber - 1; i++)
+			{
+				if (planePoints[i].size() >= 3)
+					totalArea += planeArea(planePoints[i]);
+			}
+			OGX_LINE.Format(ogx::Info, L"pole obszarów p³askich wynosi: %f", totalArea);
+#pragma endregion
+
 		}
 		, thread_count); // run with given number of threads, optional parameter, if not given will run in current thread
 	}
